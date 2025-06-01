7f0c2c71af73f59de5d1305b4126e7942072bf83
Hengli Jia <hengli0728@gmail.com>
2025-05-31 22:01:17 -0700

    Update Huffman.h

--- Honor Project/Program Files/Huffman.h
+++ Honor Project/Program Files/Huffman.h
@@ -18,9 +18,9 @@ public:
     void freeTree(HuffmanNode* node);
     HuffmanNode* build(HuffmanNode* left, HuffmanNode* right);
 
-    void printTree() const;
-    void printCode() const;
-    std::string getCode(const char ch) const;
+    void printTree() const { printTreeHelper(root, "", false); }
+    void printCode() const { printCodeHelper(root, ""); }
+    std::string getCode(const char ch) const { return getCodeHelper(root, ch, ""); }
     std::string encode(const std::string text) const;
     std::string decode(const std::string code) const;
 
@@ -29,11 +29,11 @@ public:
 
 private:
     HuffmanNode* root;
-    bool getCodeHelper(const HuffmanNode* node, char ch, std::string& path) const;
-    void encodeHelper(const HuffmanNode* node, const std::string& str, std::string codeMap[128]) const;
     void printTreeHelper(const HuffmanNode* node, const std::string& indent, bool isRight) const;
     void printCodeHelper(const HuffmanNode* node, const std::string& prefix) const;
-
+    std::string getCodeHelper(const HuffmanNode* node, char ch, std::string path) const;
+    std::string encodeHelper(const HuffmanNode* node, char ch, const std::string& path) const;
+    void decodeHelper(const HuffmanNode* node, const std::string& code, int& idx, std::string& result) const;
 };
 
 #endif // HUFFMAN_H
