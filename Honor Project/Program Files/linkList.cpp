6f4dde8434e519837323b52ca5b3eb394759f724
Hengli Jia <hengli0728@gmail.com>
2025-06-20 17:59:55 -0700

    Update linkList.cpp

--- Honor Project/Program Files/linkList.cpp
+++ Honor Project/Program Files/linkList.cpp
@@ -1,62 +1,54 @@
-#include "linkList.h"
-#include "huffman.h"
+#include "LinkedList.h"
 #include <iostream>
 
-// Node 构造函数
-Node::Node(TreeNode* t) : treeNode(t), next(nullptr) {}
+LinkedListNode::LinkedListNode(HuffmanNode* node) : huffmanNode(node), next(nullptr) {}
 
-// LinkedList 构造函数
 LinkedList::LinkedList() : head(nullptr), length(0) {}
 
-// 有序插入 TreeNode，按频率升序
-void LinkedList::insertSorted(TreeNode* treeNode) {
-    Node* newNode = new Node(treeNode);
+void LinkedList::insertSorted(LinkedListNode* newNode) {
     length++;
-    if (!head || treeNode->frequency < head->treeNode->frequency) {
+    if (!head || newNode->huffmanNode->frequency < head->huffmanNode->frequency) {
         newNode->next = head;
         head = newNode;
         return;
     }
-    Node* current = head;
-    while (current->next && current->next->treeNode->frequency <= treeNode->frequency) {
+    LinkedListNode* current = head;
+    while (current->next && current->next->huffmanNode->frequency <= newNode->huffmanNode->frequency) {
         current = current->next;
     }
     newNode->next = current->next;
     current->next = newNode;
 }
 
-// 移除前两个节点，并返回它们的 TreeNode 指针
-void LinkedList::removeFirstTwo(TreeNode*& first, TreeNode*& second) {
+void LinkedList::removeFirstTwo(HuffmanNode*& first, HuffmanNode*& second) {
     if (!head || !head->next) {
         first = nullptr;
         second = nullptr;
         return;
     }
-    Node* firstNode = head;
-    Node* secondNode = head->next;
-    first = firstNode->treeNode;
-    second = secondNode->treeNode;
+    LinkedListNode* firstNode = head;
+    LinkedListNode* secondNode = head->next;
+    first = firstNode->huffmanNode;
+    second = secondNode->huffmanNode;
     head = secondNode->next;
     delete firstNode;
     delete secondNode;
     length -= 2;
 }
 
-// 打印链表（调试用）
 void LinkedList::print() const {
-    Node* current = head;
+    LinkedListNode* current = head;
     while (current) {
-        if (current->treeNode->character != '\0')
-            std::cout << current->treeNode->character << ": ";
-        std::cout << current->treeNode->frequency << std::endl;
+        if (current->huffmanNode->character != '\0')
+            std::cout << current->huffmanNode->character << ": ";
+        std::cout << current->huffmanNode->frequency << std::endl;
         current = current->next;
     }
 }
 
-// 析构函数
 LinkedList::~LinkedList() {
     while (head) {
-        Node* temp = head;
+        LinkedListNode* temp = head;
         head = head->next;
         delete temp;
     }
